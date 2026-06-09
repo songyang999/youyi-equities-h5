8c343307206e6923d02802f388ceb82120949eb3
xiahailun <xia_hai_lun@163.com>
2026-06-09 21:52:58 +0800

    feat✨: 调首页样式

--- src/components/dialog-rule/dialog-rule.vue
+++ src/components/dialog-rule/dialog-rule.vue
@@ -6,13 +6,24 @@
 <template>
     <view class="deliver_bac" catchtouchmove="true" @click.stop>
         <view class="deliver_wrap">
-            <view class="title">订购规则</view>
-            <view class="sub_title">领券说明:</view>
+            <view class="title">权益平台隐私保护协议</view>
+            <view class="sub_title">版本号:V1.0</view>
+            <view class="sub_title">制定日期:2026年6月3日</view>
+            <view class="sub_title">生效日期:2026年6月10日</view>
+            <view class="sub_title">尊敬的用户:</view>
             <view class="content">
-                <view class="con_li">1.活动范围:中国大陆地区顺丰线上用户;在顺丰速运APP/微信小程序/公众号等线上渠道访问活动页面可参与活动;</view>
-                <view class="con_li">2.活动期间，用户成功登陆活动页面，即可查看是否有参与活动资格;</view>
-                <view class="con_li">3.成功参与活动用户，领取的优惠券为电商退货产品专用券，仅限电商退货产品使用;</view>
-                <view class="con_li">4.优惠券领取成功后将会发放到用户对应手机号的顺丰速运会员账户下;</view>
+                <view
+                    class="con_li text_indent"
+                >“新锐权益宝”（以下简称“本平台”或“我们”）深知个人信息对您的重要性，我们秉承“合法、正当、必要、诚信”的原则，采取严格的安全措施保护您的个人信息。本协议旨在向您清晰、详尽地说明我们如何收集、使用、存储、共享、转让及保护您的个人信息，以及您如何管理您的个人信息。</view>
+                <view class="con_li text_indent">请您在点击“同意”或使用我们的服务前，务必审慎阅读并充分理解本协议的全部条款。特别是以加粗、下划线或标红等方式提示您注意的条款。如您对本协议有任何疑问，可通过本协议末尾的联系方式与我们联系。</view>
+                <view class="con_li text_indent">您主动点击“同意”或通过“勾选同意”的方式确认本协议，即表示您已完全理解并同意本协议的全部内容，并授权我们按照本协议处理您的个人信息。如您不同意本协议的任何内容，请您立即停止使用本平台的服务。</view>
+                <view class="sub_title">一、定义</view>
+                <view class="con_li text_indent">1.个人信息：指以电子或者其他方式记录的与已识别或者可识别的自然人有关的各种信息，不包括匿名化处理后的信息。</view>
+                <view
+                    class="con_li text_indent"
+                >2.敏感个人信息：指一旦泄露或者非法使用，容易导致自然人的人格尊严受到侵害或者人身、财产安全受到危害的个人信息，包括但不限于生物识别、宗教信仰、特定身份、医疗健康、金融账户、行踪轨迹等信息。在本平台服务中，您提供的身份证号码及银行卡号属于敏感个人信息。</view>
+                <view class="con_li text_indent">3.权益平台服务：指由我们运营的，提供权益产品展示、订购、支付、兑换、管理等功能的小程序及关联H5页面。</view>
+                <view class="con_li text_indent">4.关联公司：指控制、受控于本平台运营方或与本平台运营方共同受控制的其他实体。</view>
             </view>
             <view class="footer_button" @click="handleClose">我知道了</view>
             <view class="close_btn" @click="handleClose">
@@ -66,7 +77,7 @@ const handleClose = () => {
             font-weight: bold;
         }
         .sub_title {
-            font-size: 32rpx;
+            font-size: 28rpx;
         }
         .title,
         .sub_title {
@@ -75,7 +86,7 @@ const handleClose = () => {
             font-weight: bold;
         }
         .content {
-            height: 524rpx;
+            height: 384rpx;
             margin-top: 18rpx;
             font-size: 28rpx;
             color: #666666;
@@ -85,6 +96,9 @@ const handleClose = () => {
                 &:last-child {
                     margin-bottom: 0;
                 }
+            }
+            .text_indent {
+                text-indent: 2em;
             }
         }
         .footer_button {
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,72 +1,65 @@
 <template>
-  <!-- <view class="content"> -->
-    <image class="banner" mode="widthFix" src="@/assets/images/banner.png" />
-    <div class="section pr">
-      <image class="bg" mode="widthFix" src="@/assets/images/bg.png" />
-      <div class="content">
-        <div class="title">资费X元/月</div>
-        <div class="sub_title" @click="handleOpen">点击按钮为同意《隐私协议》《订购规则》</div>
-        <div class="btn_box pr">
-            <template v-if="isWeixin()">
-                <wx-open-launch-weapp
-                    id="launch-btn"
-                    appid="wxae7a96d1560b3ebc"
-                    path="pages/home/index"
-                    style="display: block; width: 100%; height: 100%; background-color: orange; color: #fff; font-size: 24px; text-align: center; line-height: 47px; border-radius: 45px;"
-                >一键订购
-                    <!-- <div v-is="'script'" type='text/wxtag-template'> -->
-                        <!-- 一键订购 -->
-                        <!-- <div v-is="'style'">
-                            .applet-btn {
-                                padding: 20rpx 40rpx;
-                                font-size: 12px;
-                            }
-                        </div> -->
-                        <!-- <button class="applet-btn"></button> -->
-                    <!-- </div> -->
-                </wx-open-launch-weapp>
-            </template>
-            <div v-else @click="openMiniProgram">
-                <image class="btn_bac" src="@/assets/images/button.png"></image>
-            </div>
-        </div>
-      </div>
-    </div>
-    <div class="footer">
-      <div class="footer_card">
-        <div class="card_title">订购规则</div>
-        <div class="card_text">一、活动对象:2G/3G/4G/5G手机用户;本页面仅供客户购买权益，非用于鉴权登录等其他服务，老年及未成年人请谨慎消费，敬请知悉!为了您的权益保障，请注意核实订购规则及订单详情，不要轻信陌生人通过网络、电话、短信告知的信息谨防网络电信诈骗<br/>二、订购本产品用户，产品内所含热门会员特权可同时享有热门会员特权(N选1);热门会员特权(N选1):咪咕白金会员(月卡)爱奇艺黄金会员(月卡)、腾讯视频会员(月卡)、优酷视频会员(月卡)芒果视频会员月卡)bilibili会员(月卡)酷狗音乐会员(月卡)、Q0音乐豪华绿钻(月卡)网易云音乐会员(月卡)、搜狐视频会员(月卡)掌阅会员(月卡)、新浪微博会员(月卡)(以当月实际上线产品配置为准)等成功开通或续订本产品的用户每月可任意选择一款月会员权益进行使用;良品铺子200元代金券;三、本产品特权介绍权益说明:含腾讯视频、爱奇艺、优酷视频、芒果TV、Q0音乐等多款热门权益，成功开通或续订本产品的用户每月可任意选择一款月会员权益进行使用;权益领取有效期:自成功开通或续订本产品之日起30天内有效，请及时兑换，逾期未兑换的该权益作废。</div>
-      </div>
-    </div>
-    <dialog-rule v-if="dialogVisible" @close="handleClose" />
+    <view class="page">
+        <image class="section-img" mode="widthFix" src="@/assets/images/video_1.png" />
+
+        <image class="section-img" mode="widthFix" src="@/assets/images/video_2.png" />
+
+        <image class="section-img" mode="widthFix" src="@/assets/images/video_3.png" />
+
+        <image class="section-img" mode="widthFix" src="@/assets/images/video_4.png" />
+
+        <image class="section-img" mode="widthFix" src="@/assets/images/video_5.png" />
+        <view class="footer">
+            <view class="btn-box">
+                <template v-if="isWeixin()">
+                    <wx-open-launch-weapp
+                        id="launch-btn"
+                        appid="wxae7a96d1560b3ebc"
+                        path="pages/home/index"
+                        style="width: 542rpx;height: 90rpx;display: block; background-color: orange; color: #fff; font-size: 24px; text-align: center; line-height: 90rpx; border-radius: 50px; margin-bottom: 20rpx;"
+                    >
+                        <!-- <template> -->
+                        一键订购
+                        <!-- <img style="width: 542rpx;height: 90rpx;display: block;" src="@/assets/images/button.png" /> -->
+                        <!-- </template> -->
+                    </wx-open-launch-weapp>
+                </template>
+                <view v-else class="order-btn" @click="openMiniProgram">
+                    <image class="btn-bg" src="@/assets/images/button.png" mode="widthFix" />
+                </view>
+            </view>
+            <view class="footer-text">
+                点击按钮视为同意
+                <text @click="handleOpen(1)">《隐私协议》</text>及
+                <text @click="handleOpen(2)">《扣款授权确认书》</text>
+            </view>
+        </view>
+
+        <dialog-rule v-if="dialogVisible" @close="handleClose" />
+    </view>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useUserStore } from '@/store/modules/user'
 import { wxConfig } from '@/utils/wechat'
 import { isWeixin } from '@/utils/index'
 import { getUrlScheme } from '@/api/index'
+
 const title = useUserStore().username
+
 onMounted(() => {
-//   if (isWeixin()) {
     wxConfig()
-//   } else {
-//     getScheme()
-//   }
 })
+
 const openMiniProgram = () => {
     console.log('打开小程序')
-//   uni.navigateTo({
-//     url: '/pages/miniProgram/index',
-//   })
 }
 
 const getScheme = async () => {
     try {
         const res = await getUrlScheme({ url: '/pages/home/index' })
         console.log('res', res)
-        
     } catch (error) {
         //
     }
@@ -82,77 +75,54 @@ const handleClose = () => {
 </script>
 
 <style lang="scss" scoped>
-.banner,
-.bg {
-  display: block;
-  width: 100%;
-}
-.content {
-  width: 100%;
-  height: 400rpx;
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+.page {
+    background: #fff;
+    min-height: 100vh;
 }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 38rpx;
-  color: #d84315;
-  font-weight: bold;
-  margin-top: 116rpx;
-}
-.sub_title {
-  font-size: 22rpx;
-  color: #d84315;
-  letter-spacing: 10rpx;
-  margin-top: 10rpx;
-}
-.btn_box {
-  width: 332rpx;
-  height: 90rpx;
-  margin-top: 52rpx;
-  .btn_bac {
-    display: block;
-    width: 332rpx;
-    height: 90rpx;
-  }
-}
-.footer {
-  width: 100%;
-  box-sizing: border-box;
-  padding: 24rpx 30rpx 104rpx;
-  background: #f35536;
-  .footer_card {
+.section-img {
     width: 100%;
-    padding: 40rpx;
-    border-radius: 30rpx;
-    background: linear-gradient(120deg, #fff0f2, #faefd0);
-    color: #d84315;
-    text-align: center;
-    .card_title {
-      font-size: 44rpx;
-    }
-    .card_text {
-      font-size: 30rpx;
-      margin-top: 46rpx;
-    }
-  }
+    display: block;
+}
+
+.bottom-section {
+    padding: 30rpx;
+    background: linear-gradient(135deg, #ff6b35, #ff4500);
+    margin: 20rpx;
+    border-radius: 24rpx;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.order-btn {
+    display: block;
+    margin-bottom: 20rpx;
+}
+
+.btn-bg {
+    width: 542rpx;
+    height: 90rpx;
+    display: block;
+}
+
+.footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #fe654e;
+    height: 220rpx;
+    box-sizing: border-box;
+    padding: 26rpx 30rpx 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    // justify-content: center;
+}
+
+.footer-text {
+    font-size: 24rpx;
+    color: #fff;
 }
 </style>
